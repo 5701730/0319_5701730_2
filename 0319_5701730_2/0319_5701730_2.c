204d3f36b49e9a60a3035ff53d038aaeea2cb38f
8933hkt3618@gmail.com <8933hkt3618@gmail.com>
2024-03-21 01:07:13 +0900

    초기 커밋.

---
+++ 0319_5701730_2/0319_5701730_2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+
+long long power_liter(long x, int n) {
+    int i;
+    long result = 1;
+    for (i = 0; i < n; i++)
+        result = result * x;
+    return result;
+}
+
+long long power_rec(long x, int n) {
+    if (n == 0)
+        return 1;
+    else if (n % 2 == 0)
+        return power_rec(x * x, n / 2);
+    else
+        return x * power_rec(x * x, (n - 1) / 2);
+}
+
+int main(int argc, char* argv[]) {
+    clock_t start, end;
+    start = clock();
+    long long result_liter = power_liter(13, 21);
+    end = clock();
+    printf("Power Iterative Result: %lld\n", result_liter);
+    printf("Time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    start = clock(); // 시간 측정을 위해 start 변수 재사용
+    long long result_rec = power_rec(13, 21);
+    end = clock();
+    printf("Power Recursive Result: %lld\n", result_rec);
+    printf("Time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    return 0;
+}
