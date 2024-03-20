60dac56098ecfb9b0fdb26df9bb7257e4f942b29
8933hkt3618@gmail.com <8933hkt3618@gmail.com>
2024-03-21 01:22:57 +0900

    third commit.

--- 0319_5701730_2/0319_5701730_2.c
+++ 0319_5701730_2/0319_5701730_2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <time.h>
 
-long long power_liter(long x, int n) {//반복 함수를 이용
+double power_liter(double x, int n) {//반복 함수를 이용 정수 반환 하니깐 값이 출력이 안됨 double로 변경
     int i;
-    long result = 1;
+    double result = 1;
     for (i = 0; i < n; i++)
         result = result * x;
     return result;
 }
 
-long long power_rec(long x, int n) {//재귀 함수를 이용
+double power_rec(double x, int n) {//재귀 함수를 이용
     if (n == 0)
         return 1;
     else if (n % 2 == 0)
@@ -21,15 +21,15 @@ long long power_rec(long x, int n) {//재귀 함수를 이용
 int main(int argc, char* argv[]) {
     clock_t start, end;
     start = clock();
-    long long result_liter = power_liter(13, 21);
+    double result_liter = power_liter(13, 21);
     end = clock();
-    printf("Power Iterative Result: %lld\n", result_liter);
+    printf("Power Iterative Result: %lf\n", result_liter);
     printf("Time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     start = clock(); // 시간 측정을 위해 start 변수 재사용
-    long long result_rec = power_rec(13, 21);
+    double result_rec = power_rec(13, 21);
     end = clock();
-    printf("Power Recursive Result: %lld\n", result_rec);
+    printf("Power Recursive Result: %lf\n", result_rec);
     printf("Time: %lf seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
